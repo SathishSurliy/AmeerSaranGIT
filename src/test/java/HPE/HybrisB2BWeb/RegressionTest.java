368b674766c8fb9fe7aaabb8148832a048e11f83
SathishSurliy <sathishgit86@gmail.com>
2022-06-02 22:01:57 +0530

    commiting smoke&regressionTests

---
+++ src/test/java/HPE/HybrisB2BWeb/RegressionTest.java
@@ -0,0 +1,55 @@
+package HPE.HybrisB2BWeb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+	
+	
+@Test
+	
+	void test1() {
+		Assert.assertTrue(true);
+	}
+	
+@Test
+	
+	void test2() {
+	Assert.assertTrue(true);	}
+@Test
+
+void test3() {
+	Assert.assertTrue(true);}
+@Test
+
+void test4() {
+	Assert.assertTrue(true);}
+@Test
+
+void tes5() {
+	Assert.assertTrue(true);}
+
+
+@Test
+
+void test6() {
+	Assert.assertTrue(false);
+}
+
+@Test
+
+void test7() {
+Assert.assertTrue(true);	}
+@Test
+
+void test8() {
+Assert.assertTrue(true);}
+@Test
+
+void test9() {
+Assert.assertTrue(true);}
+@Test
+
+void tes10() {
+Assert.assertTrue(false);}
+}
---
+++ src/test/java/HPE/HybrisB2BWeb/SmokeTest.java
@@ -0,0 +1,36 @@
+package HPE.HybrisB2BWeb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+
+
+public class SmokeTest {
+	
+	
+	@Test
+	
+	void test1() {
+		Assert.assertTrue(true);
+	}
+	
+@Test
+	
+	void test2() {
+	Assert.assertTrue(true);	}
+@Test
+
+void test3() {
+	Assert.assertTrue(true);}
+@Test
+
+void test4() {
+	Assert.assertTrue(true);}
+@Test
+
+void tes5() {
+	Assert.assertTrue(true);}
+
+
+}
